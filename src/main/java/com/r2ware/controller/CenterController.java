6b366b6f3fa59b4fa20a758cd52c8e6964525647
Jae Sung, Choi <cjaesung@gmail.com>
2020-12-28 03:59:08 +0900

    remove Member.java, MemberRepository.java

--- src/main/java/com/r2ware/controller/CenterController.java
+++ src/main/java/com/r2ware/controller/CenterController.java
@@ -1,7 +1,5 @@
 package com.r2ware.controller;
 
-import com.r2ware.repository.Member;
-import com.r2ware.repository.MemberRepository;
 import com.r2ware.repository.Users;
 import com.r2ware.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Controller
--- src/main/java/com/r2ware/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.r2ware.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
